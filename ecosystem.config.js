f95b9fcbc35a3c86acdd5c1e500d7ab978431eff
Makcy <644215038@qq.com>
2017-09-01 14:57:19 +0800

    [FIX]

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,18 +29,18 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/Makcy/reacy.git',
       path : '/var/www/production',
-      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production && pm2 stop reacy -f && pm2 start app.js -n reacy'
     },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
+    // dev : {
+    //   user : 'node',
+    //   host : '212.83.163.1',
+    //   ref  : 'origin/master',
+    //   repo : 'git@github.com:repo.git',
+    //   path : '/var/www/development',
+    //   'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+    //   env  : {
+    //     NODE_ENV: 'dev'
+    //   }
+    // }
   }
 };
