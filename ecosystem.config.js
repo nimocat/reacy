4c9cf6743ace43efa081742e9563bcc287e734e9
Makcy <644215038@qq.com>
2017-09-06 16:27:08 +0800

    04:27

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:Makcy/reacy.git',
       path : '/var/www/production',
-      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'yarn install && pm2 startOrReload ecosystem.config.js --env production'
     },
   }
 };
