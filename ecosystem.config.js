fc1c28ada123d25d156d32c9bf9af50b763f66bf
Makcy <644215038@qq.com>
2017-09-06 16:45:48 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,10 @@ module.exports = {
       script : './api/app.js',
       env : {
         // COMMON_VARIABLE: 'true'
+        // instances: 3,
+        // exec_mode: 'cluster',
+        // merge_logs: true,
       }
-      // instances: 3,
-      // exec_mode: 'cluster',
-      // merge_logs: true,
       env_production : {
         NODE_ENV: 'production'
       }
@@ -21,7 +21,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:Makcy/reacy.git',
       path : '/var/www/production',
-      'post-deploy' : 'yarn install && pm2 startOrReload ecosystem.config.js --env production'
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
     },
   }
 };
